3aeb5a1949caca39503482d1dea0fa1ac017b055
Ye WangDa <yewangda@qq.com>
2021-08-09 02:59:09 +0800

    第四周作业

--- week04/product/internal/data/product.go
+++ week04/product/internal/data/product.go
@@ -2,7 +2,6 @@ package data
 
 import (
 	"context"
-	"encoding/json"
 	"product/internal/biz"
 
 	"github.com/go-kratos/kratos/v2/log"
@@ -29,52 +28,54 @@ type product struct {
 	Price       float64 `db:"price"`
 }
 
-func pToPB(product product) (*biz.Product, error) {
-	pj, err := json.Marshal(product)
-	if err != nil {
-		return nil, errors.Wrapf(err, "product: %v\n", product)
+func pTod(product product) *biz.Product {
+	// pj, err := json.Marshal(product)
+	// if err != nil {
+	// 	return nil, errors.Wrapf(err, "product: %v\n", product)
+	// }
+	// pb := new(biz.Product)
+	// err = json.Unmarshal(pj, pb)
+	// if err != nil {
+	// 	return nil, errors.Wrapf(err, "pj: %v, pb: %v\n", pj, pb)
+	// }
+	// return pb, nil
+	return &biz.Product{
+		Id:          product.Id,
+		Name:        product.Name,
+		Description: product.Description,
+		Price:       product.Price,
 	}
-	pb := new(biz.Product)
-	err = json.Unmarshal(pj, pb)
-	if err != nil {
-		return nil, errors.Wrapf(err, "pj: %v, pb: %v\n", pj, pb)
-	}
-	return pb, nil
 }
 
 func (r *productRepo) ListProduct(ctx context.Context) ([]*biz.Product, error) {
+	// TODO 处理ctx
 	sqlStr := "SELECT id, name, description, price FROM product"
 	var products []product
 	if err := r.data.db.Select(&products, sqlStr); err != nil {
 		return nil, errors.Wrapf(err, "sql: %s", sqlStr)
 	}
-
 	pbs := make([]*biz.Product, len(products), len(products))
 	for i := 0; i < len(products); i++ {
-		pb, err := pToPB(products[i])
-		if err != nil {
-			return nil, err
-		}
-		pbs = append(pbs, pb)
+		pb := pTod(products[i])
+		pbs[i] = pb
 	}
 	return pbs, nil
 }
 
 func (r *productRepo) GetProduct(ctx context.Context, id int64) (*biz.Product, error) {
+	// TODO 处理ctx
 	sqlStr := "SELECT id, name, description, price FROM product WHERE id = ?"
 	var p product
 	if err := r.data.db.Get(&p, sqlStr, id); err != nil {
 		return nil, errors.Wrapf(err, "sql: %s", sqlStr)
 	}
 
-	pb, err := pToPB(p)
-	if err != nil {
-		return nil, err
-	}
+	pb := pTod(p)
 	return pb, nil
 }
 
 func (r *productRepo) CreateProduct(ctx context.Context, p *biz.Product) (int64, error) {
+	// TODO 处理ctx
 	sqlStr := "INSERT INTO product(name, description, price) VALUES(?, ?, ?)"
 	result, err := r.data.db.Exec(sqlStr, p.Name, p.Description, p.Price)
 	if err != nil {
@@ -88,8 +89,9 @@ func (r *productRepo) CreateProduct(ctx context.Context, p *biz.Product) (int64,
 }
 
 func (r *productRepo) UpdateProduct(ctx context.Context, id int64, p *biz.Product) error {
+	// TODO 处理ctx
 	sqlStr := "UPDATE product SET description = ?, price= ? WHERE id = ?"
-	_, err := r.data.db.Exec(sqlStr, id, p.Description, p.Price)
+	_, err := r.data.db.Exec(sqlStr, p.Description, p.Price, id)
 	if err != nil {
 		return errors.Wrapf(err, "sql: %s", sqlStr)
 	}
@@ -97,6 +99,7 @@ func (r *productRepo) UpdateProduct(ctx context.Context, id int64, p *biz.Produc
 }
 
 func (r *productRepo) DeleteProduct(ctx context.Context, id int64) error {
+	// TODO 处理ctx
 	sqlStr := "DELETE FROM product WHERE id = ?"
 	_, err := r.data.db.Exec(sqlStr, id)
 	if err != nil {
--- week04/product/internal/service/product.go
+++ week04/product/internal/service/product.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 	"product/internal/biz"
 
 	"github.com/pkg/errors"
@@ -21,6 +20,7 @@ func NewProductService(product *biz.ProductUsecase, logger log.Logger) *ProductS
 
 func (s *ProductService) CreateProduct(ctx context.Context, req *pb.CreateProductRequest) (*pb.CreateProductReply, error) {
 	s.log.WithContext(ctx).Infof("CreateProduct Received: %v", req)
+	// TODO 修改验证与错误处理
 	if req.Name == "" {
 		return nil, errors.New("未设置名称")
 	}
@@ -36,35 +36,103 @@ func (s *ProductService) CreateProduct(ctx context.Context, req *pb.CreateProduc
 		Price:       req.Price,
 	})
 	if err != nil {
-		return nil, err
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.CreateProduct error: \n%+v\n", err)
+		return nil, errors.New("保存商品失败")
 	}
-	fmt.Printf("insert id: %d\n", id)
 	p, err := s.product.Get(ctx, id)
 	if err != nil {
-		return nil, err
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.CreateProduct error: \n%+v\n", err)
+		return nil, errors.New("查询商品失败")
 	}
 	return &pb.CreateProductReply{
-		Product: &pb.ProductInfo{
-			Id:          p.Id,
-			Name:        p.Name,
-			Description: p.Description,
-			Price:       p.Price,
-		},
+		Product: s.DTO(p),
 	}, nil
 }
 func (s *ProductService) UpdateProduct(ctx context.Context, req *pb.UpdateProductRequest) (*pb.UpdateProductReply, error) {
 	s.log.WithContext(ctx).Infof("UpdateProduct Received: %v", req)
+	// TODO 修改验证与错误处理
+	if req.Id <= 0 {
+		return nil, errors.New("商品Id不能为空")
+	}
+	if req.Description == "" {
+		return nil, errors.New("未设置描述")
+	}
+	if req.Price == 0.0 {
+		return nil, errors.New("价格不能为0")
+	}
+	err := s.product.Update(ctx, req.Id, &biz.Product{
+		Description: req.Description,
+		Price:       req.Price,
+	})
+	if err != nil {
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.UpdateProduct error: \n%+v\n", err)
+		return nil, errors.New("更新商品失败")
+	}
 	return &pb.UpdateProductReply{}, nil
 }
 func (s *ProductService) DeleteProduct(ctx context.Context, req *pb.DeleteProductRequest) (*pb.DeleteProductReply, error) {
 	s.log.WithContext(ctx).Infof("DeleteProduct Received: %v", req.GetId())
+	// TODO 修改验证与错误处理
+	if req.Id <= 0 {
+		return nil, errors.New("商品Id不能为空")
+	}
+	err := s.product.Delete(ctx, req.Id)
+	if err != nil {
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.DeleteProduct error: \n%+v\n", err)
+		return nil, errors.New("删除商品失败")
+	}
 	return &pb.DeleteProductReply{}, nil
 }
 func (s *ProductService) GetProduct(ctx context.Context, req *pb.GetProductRequest) (*pb.GetProductReply, error) {
 	s.log.WithContext(ctx).Infof("GetProduct Received: %v", req.GetId())
-	return &pb.GetProductReply{}, nil
+	// TODO 修改验证与错误处理
+	if req.Id <= 0 {
+		return nil, errors.New("商品Id不能为空")
+	}
+	p, err := s.product.Get(ctx, req.Id)
+	if err != nil {
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.GetProduct error: \n%+v\n", err)
+		return nil, errors.New("查询商品失败")
+	}
+	return &pb.GetProductReply{
+		Product: s.DTO(p),
+	}, nil
 }
 func (s *ProductService) ListProduct(ctx context.Context, req *pb.ListProductRequest) (*pb.ListProductReply, error) {
 	s.log.WithContext(ctx).Infof("ListProduct Received: %v", req)
-	return &pb.ListProductReply{}, nil
+	ps, err := s.product.List(ctx)
+	if err != nil {
+		s.log.WithContext(ctx).Errorf("product.internal.service.product.ListProduct error: \n%+v\n", err)
+		return nil, errors.New("查询列表出错")
+	}
+	return &pb.ListProductReply{
+		Results: s.DTOs(ps),
+	}, nil
+}
+
+func (s ProductService) DTO(p *biz.Product) *pb.ProductInfo {
+	if p == nil {
+		return nil
+	}
+	return &pb.ProductInfo{
+		Id:          p.Id,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+	}
+}
+
+func (s ProductService) DTOs(ps []*biz.Product) []*pb.ProductInfo {
+	if ps == nil {
+		return nil
+	}
+	pbs := make([]*pb.ProductInfo, len(ps), len(ps))
+	for i := 0; i < len(ps); i++ {
+		pb := s.DTO(ps[i])
+		if pb == nil {
+			continue
+		}
+		pbs[i] = pb
+	}
+	return pbs
 }
